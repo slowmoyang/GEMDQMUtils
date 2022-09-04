5753959411052c8a225b351657e9c20cb379ba47
Seungjin Yang <slowmoyang@gmail.com>
2022-09-04 16:52:07 +0900

    add a function to retrieve t0 config

---
+++ gemdqm/t0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import requests
+from bs4 import BeautifulSoup
+from http import HTTPStatus
+import json
+
+def get_t0_config(run_number: int, express: bool):
+    reco_type = "express" if express else "reco"
+    url = f'https://cmsweb.cern.ch/t0wmadatasvc/prod/{reco_type}_config?run={run_number}'
+    response = requests.get(url, verify=False)
+    if response.status_code != HTTPStatus.OK:
+        raise RuntimeError(f'[{response.status_code=}] {url=:s}')
+    soup = BeautifulSoup(response.text, features="html.parser")
+    config = json.loads(soup.contents[0]) # type: ignore
+    return config
